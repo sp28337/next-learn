d6fe893897234361fe97d5773e5e79362205bc29
Pavel Tarakanov <pavel191094@icloud.com>
2025-03-25 20:12:10 +0300

    Best practice: Debouncing

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useDebouncedCallback } from 'use-debounce';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
@@ -9,8 +10,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term) =>  {
     
+    console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
     
     if (term) {
@@ -20,7 +22,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
